ffc25eecfc8b5eff4dfdbf1bf23875a71f659bea
SipuliSopuli <timi.wahalahti@gmail.com>
2013-04-08 18:07:44 +0300

    Initial commit

---
+++ mustapaiva.js
@@ -0,0 +1,74 @@
+var copyrightCampaign = function(options) {
+    options = typeof options !== 'undefined' ? options : {};
+    function setDefaultOption(option, value) {
+        options[option] = typeof options[option] !== 'undefined' ? options[option] : value;
+    }
+
+    setDefaultOption(options, {});
+    setDefaultOption("onCampaignDayOnly", true);
+    setDefaultOption("showOnlyOnce", true);
+    setDefaultOption("showCoders", false);
+    setDefaultOption("title", 'Järkeä tekijänoikeuslakiin?');
+    setDefaultOption("bigText", 'Allekirjoita <a href="https://www.kansalaisaloite.fi/fi/aloite/70" target="_blank">kansalaisaloite kohtuullisemman tekijänoikeuslain puolesta</a>.');
+    setDefaultOption("smallText", 'Katso myös: <a href="#">Internetin musta päivä 23.4.</a>');
+
+    console.log(options);
+
+    var today = new Date();
+    if (!(today.getDate() == 23 && today.getMonth() == 3) && options["onCampaignDayOnly"]) {
+        return;
+    }
+
+    if (typeof jQuery === "undefined") {
+        var script_tag = document.createElement('script');
+        script_tag.setAttribute("type","text/javascript");
+        script_tag.setAttribute("src",
+          "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js");
+        script_tag.onload = main; // Run main() once jQuery has loaded
+        script_tag.onreadystatechange = function () { // Same thing but for IE
+          if (this.readyState == 'complete' || this.readyState == 'loaded') main();
+        }
+        document.getElementsByTagName("head")[0].appendChild(script_tag);
+    } else {
+        main();
+    }
+
+    function main() {
+        jQuery(document).ready(function (e) {
+            jQuery("head").prepend("<link href='http://fonts.googleapis.com/css?family=Open+Sans:800|Gentium+Book+Basic' rel='stylesheet' type='text/css'>");
+            
+            var widgetStyle = '<style type="text/css">#copyright-campaign-box { z-index:1000;position:fixed;top:0;left:0;width:100%;height:100%;background:rgb(0,0,0);background:rgba(0,0,0,0.9);text-align:center;font-family:\'Gentium Book Basic\', serif;letter-spacing:0.1em;font-size:1em; } .inner-campaign-box { position:fixed;top:28px;right:28px;bottom:28px;left:28px;color:#EFEFEF;padding:20px;border:5px solid #ffffff; } #corner-close-button { position:fixed;top:8px;right:8px;padding:8px 16px;border-radius:2em;text-decoration:none;border:5px solid #ffffff;color:#ffffff;background:#000000;font-size:1em;font-weight:bold; } #corner-close-button:hover { background:#333333; } #campaign-text-content { position:fixed;top:30%;left:0;width:100%; } #copyright-campaign-box h1 { width:100%;font-family:\'Open Sans\', sans-serif;text-transform:uppercase;text-align:center;font-size:2.5em;margin: 0 auto 40px auto; } .campaign-text-content div { max-width:25em; padding: 0 30px; margin: 0 auto 40px auto; } #copyright-campaign-box a { color:#aaaacc;text-decoration:none; } #copyright-campaign-box a:hover { color:#ccccee;text-decoration:none; } #copyright-campaign-box .subscribe { font-size:1.5em; } #copyright-campaign-box .credits { position:fixed;bottom:40px;left:0;text-align:center;width:100%; } @media (max-width: 680px) { #copyright-campaign-box, #copyright-campaign-box .subscribe { font-size:1em; } #copyright-campaign-box h1 { font-size:1.5em; } .campaign-text-content { top: 20% } .campaign-text-content div, #copyright-campaign-box h1 { margin-bottom: 20px; } } @media(max-height: 500px) { .campaign-text-content { top: 10%; } .campaign-text-content div, #copyright-campaign-box h1 { margin-bottom: 10px; max-width: 35em } #copyright-campaign-box .credits { position: static;width:auto;max-width:none;} } </style>';
+
+            jQuery("head").prepend(widgetStyle);
+
+            var coders = '';
+            if (options['showCoders']) {
+                var coders = '<div class="credits"><a href="https://github.com/SC5/copyright-campaign/">Code</a> by <a href="http://sc5.io" target="_blank">SC5</a> and <a href="http://wahalahti.fi" target="_blank">Wahalahti</a></div>';
+            };
+
+            var widgetElement = '<div id="copyright-campaign-box"><div class="inner-campaign-box";><a href="#" id="corner-close-button" class="close-copyright-campaign-box" style="color:#ffffff">Sulje</a><div class="campaign-text-content"><h1>'+options.title+'</h1><div class="subscribe">'+options.bigText+'</div><div>'+options.smallText+'</div><div><a href="#" class="close-copyright-campaign-box">Sulje</a></div></div>'+coders+'</div></div>';
+            jQuery(document.body).append(widgetElement);
+
+            function hideBox() {
+                jQuery("#copyright-campaign-box").hide();
+                if(options["showOnlyOnce"] && typeof(Storage)!=="undefined") {
+                    localStorage.boxClosed = true;
+                }
+            }
+
+            if(options["showOnlyOnce"] && typeof(Storage)!=="undefined" && localStorage.boxClosed) {
+                hideBox();
+            }
+
+            jQuery(".close-copyright-campaign-box").click(function(e) {
+                hideBox();
+            });
+
+            jQuery(document).keyup(function(e) {
+                if (e.keyCode == 27) {
+                    hideBox();
+                }
+            });
+        });
+    }
+};
---
+++ mustapaiva.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Plugin Name: Internetin musta päivä
+ * Plugin URI: https://www.facebook.com/events/160986290729976/
+ * Description: Lisää sivullesi SC5:en tekemän koodin jolla osallistut Internetin mustaan päivään. Voit huoletta asentaa ja aktivoida lisäosan jo nyt, se muuttaa sivustoasi vasta kampanjapäivänä 23.4. Muokkaa asetuksia 'Asetukset' -välilehdellä 
+ * Version: 1.0
+ * Author: Timi Wahalahti
+ * Author URI: http://wahalahti.fi
+ */
+
+
+add_action('admin_init', 'mustapaiva_opt_init_fn' );
+add_action('admin_menu', 'mustapaiva_opt_add_page_fn');
+
+function mustapaiva_opt_init_fn(){
+	register_setting('mustapaiva_options', 'mustapaiva_options', 'mustapaiva_options_validate' );
+	add_settings_section('main_section', 'Asetukset', 'section_text_fn', __FILE__);
+	add_settings_field('only', 'Näytä joka päivä:<br><small>Oletuksena näytetään vain 23.4.</small>', 'setting_string_only', __FILE__, 'main_section');
+	add_settings_field('once', 'Näytä useammin kuin kerran:<br><small>Oletuksena näytetään vain kerran</small>', 'setting_string_once', __FILE__, 'main_section');
+	add_settings_field('title', 'Otsikko:', 'setting_string_title', __FILE__, 'main_section');
+	add_settings_field('big_text', 'Iso teksti:', 'setting_string_big_text', __FILE__, 'main_section');
+	add_settings_field('small_text', 'Pieni teksti:', 'setting_string_small_text', __FILE__, 'main_section');
+	add_settings_field('coder', 'Näytä tekijät:<br><small>Näytä ikkunan alaosassa tekijät</small>', 'setting_string_coder', __FILE__, 'main_section');
+}
+
+function mustapaiva_opt_add_page_fn() {
+	add_options_page('Internetin musta päivä', 'Internetin musta päivä', 'administrator', __FILE__, 'mustapaiva_opt_page_fn');
+}
+
+
+function  section_text_fn() {
+	echo "<p>Oletusarvoisesti koodi aktivoituu vain kampanjapäivänä 23.4. ja näkyy jokaiselle sivuston kävijälle vain kerran.</p>";
+	echo '<p>Jos haluat, voit muokata tekstejä jotka näkyy kampanjapäivänä.<br>Voit käyttää HTML-muotoilua, esimerkiksi linkki lisätään näin: &#60;a href="https://www.kansalaisaloite.fi/fi/aloite/70" target="_blank"&#62;kansalaisaloite&#60;/a&#62;. </p>';
+	echo "<p>Jos muokkaat tekstejä, aktivoi 'Näytä joka päivä' ja tarkista muutokset.</p>";
+}
+
+function setting_string_only() {
+	$options = get_option('mustapaiva_options');
+	echo "<input type='checkbox' name='mustapaiva_options[only]'' value='1'" .checked( isset( $options['only'] ), true, false ). "/>";
+}
+
+function setting_string_once() {
+	$options = get_option('mustapaiva_options');
+	echo "<input type='checkbox' name='mustapaiva_options[once]'' value='1'" .checked( isset( $options['once'] ), true, false ). "/>";
+}
+
+function setting_string_title() {
+	$options = get_option('mustapaiva_options');
+	echo "<input id='title' name='mustapaiva_options[title]' size='40' type='text' value='{$options['title']}' />";
+}
+
+function setting_string_big_text() {
+	$options = get_option('mustapaiva_options');
+	echo "<input id='big_text' name='mustapaiva_options[big_text]' size='40' type='text' value='{$options['big_text']}' />";
+}
+
+function setting_string_small_text() {
+	$options = get_option('mustapaiva_options');
+	echo "<input id='small_text' name='mustapaiva_options[small_text]' size='40' type='text' value='{$options['small_text']}' />";
+}
+function setting_string_coder() {
+	$options = get_option('mustapaiva_options');
+	echo "<input type='checkbox' name='mustapaiva_options[coder]'' value='1'" .checked( isset( $options['coder'] ), true, false ). "/>";
+}
+
+function mustapaiva_opt_page_fn() {
+?>
+	<div class="wrap">
+		<div class="icon32" id="icon-options-general"><br></div>
+		<h2>Internetin musta päivä</h2>
+		<form action="options.php" method="post">
+		<?php settings_fields('mustapaiva_options'); ?>
+		<?php do_settings_sections(__FILE__); ?>
+		<p class="submit">
+			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
+		</p>
+		</form>
+	</div>
+<?php
+}
+
+function mustapaiva_options_validate($input) {
+	$input['title'] =  wp_kses($input['title']);
+	$input['big_text'] =  wp_kses($input['big_text']);
+	$input['small_text'] =  wp_kses($input['small_text']);	
+	return $input;
+}
+
+function mustapaiva_add() {
+$options = get_option('mustapaiva_options');
+echo "<script type='text/javascript' src='" .plugins_url( 'mustapaiva.js' , __FILE__ ). "' charset='UTF-8'></script>";
+
+if (isset($options['only'] )) {
+	$hihi = "onCampaignDayOnly: false, ";
+}
+if (isset( $options['once'] )) {
+	$hihi .= "showOnlyOnce: false, ";
+}
+if (isset( $options['coder'] )) {
+	$hihi .= "showCoders: true, ";
+}
+if (!empty($options['title'])) {
+	$hihi .= "title: '". $options['title']. "', ";
+}
+if (!empty($options['big_text'])) {
+	$hihi .= "bigText: '". $options['big_text']. "', ";
+}
+if (!empty($options['small_text'])) {
+	$hihi .= "smallText: '". $options['small_text']. "'";
+}
+
+?>
+<script>
+copyrightCampaign({ <?php echo $hihi; ?> });
+</script>
+<?php
+}
+add_action('wp_head', 'mustapaiva_add');
+?>
